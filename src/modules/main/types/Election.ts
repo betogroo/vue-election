e77fb2b9411d097098f5e6c9d271c42195149bc5
betogroo <luizhumberto@gmail.com>
2024-02-26 08:58:00 -0300

    feat: create election types and validation #4

---
+++ src/modules/main/types/Election.ts
@@ -0,0 +1,32 @@
+import { z } from 'zod'
+
+import {
+  FORM_REQUIRED as required_error,
+  FORM_NUMBER_ONLY as form_number_only,
+} from '@/config'
+
+const requiredStringField = () => z.string({ required_error })
+
+export const baseElectionSchema = z.object({
+  id: requiredStringField().uuid(),
+  created_at: z.string(),
+  date: requiredStringField(),
+  name: requiredStringField(),
+  description: requiredStringField(),
+  uppercase: z.boolean().default(false),
+  ready: z.boolean().default(false),
+  organization: requiredStringField(),
+  candidate_number_length: z
+    .number({ invalid_type_error: form_number_only, required_error })
+    .min(1, 'O número deve ser entre 1 e 5')
+    .max(5, 'O número deve ser entre 1 e 5'),
+})
+
+export const insertElectionSchema = baseElectionSchema.omit({
+  id: true,
+  created_at: true,
+})
+
+export type Election = z.infer<typeof baseElectionSchema>
+
+export type ElectionInsert = z.infer<typeof insertElectionSchema>
