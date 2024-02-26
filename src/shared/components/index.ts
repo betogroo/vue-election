c8697af2a226fe10df6525ff5f8f77978bc822d5
betogroo <luizhumberto@gmail.com>
2024-02-26 16:38:25 -0300

    feat: create app loading

--- src/shared/components/index.ts
+++ src/shared/components/index.ts
@@ -1,4 +1,5 @@
 import AppBar from './app/AppBar.vue'
 import AppFileBtn from './app/AppFileBtn.vue'
+import AppLoading from './app/AppLoading.vue'
 
-export { AppBar, AppFileBtn }
+export { AppBar, AppFileBtn, AppLoading }
