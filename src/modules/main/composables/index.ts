3ee2de972b18dc8abbe212c3face52322fb0e456
betogroo <luizhumberto@gmail.com>
2024-03-15 08:29:36 -0300

    feat: create use voters composable #34

--- src/modules/main/composables/index.ts
+++ src/modules/main/composables/index.ts
@@ -1,7 +1,8 @@
 import useBallotBox from './useBallotBox'
 import useCandidates from './useCandidates'
 import useElection from './useElection'
+import useVoters from './useVoters'
 
 import { useHelpers } from '@/shared/composables'
 
-export { useCandidates, useBallotBox, useElection, useHelpers }
+export { useCandidates, useBallotBox, useVoters, useHelpers, useElection }
